0a58f2fb6a12d020034fc6ac6ae65d2c02d33c22
matt vile <matt.vile@yahoo.co.uk>
2021-02-18 16:52:22 +0000

    did not impliment show or index correctlu

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+# A user can see the list of cocktails
+root to: "cocktails#index"
+resources :cocktails, only: [:index, :new,:show,:create]
+# A user can see the details of a given cocktail, with the dose needed for each ingredient
+# GET "cocktails/42"
+# A user can create a new cocktail
+# GET "cocktails/new"
+# POST "cocktails"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
